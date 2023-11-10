f39875395cabcf2ca84183f67df2438770bc4a85
xcr15 <120267628+xcr15@users.noreply.github.com>
2023-11-10 12:52:21 -0500

    Update main.js
    stopped users from opening the new webstore link if their version is previous to when it was added

--- new-tab/main.js
+++ new-tab/main.js
@@ -13,26 +13,27 @@ let [
     addAccount,
     move,
     fullscreen
-] = document.querySelectorAll('svg')
-
+] = document.querySelectorAll('svg');
 files.addEventListener('click', () => {
     chrome.tabs.create({}, (tab) => {
-        chrome.tabs.update(tab.id, { url: FILES_URL })
-    })
-})
+        chrome.tabs.update(tab.id, { url: FILES_URL });
+    });
+});
 
 help.addEventListener('click', () => {
-    chrome.tabs.create({ url: HELP_URL })
-})
+    chrome.tabs.create({ url: HELP_URL });
+});
 
 webStore.addEventListener('click', () => {
     let version = Number(navigator.appVersion.match(/Chrom(e|ium)\/([0-9]+)/)[2]);
-    if (version < 112) { // not sure if this is actually the version
-        alert("This web store may not supported by your version");
+    if (version < 112) { 
+        // not sure if this is actually the version
+        return alert("This web store is not supported by your version");
+        // if it doesn't work on old versions then why would we give them a broken link, regardless if we tell them it may not work, it's better to just not let them visit the link instead - aka.
     }
 
-    chrome.tabs.create({ url: WEBSTORE_URL })
-})
+  chrome.tabs.create({ url: WEBSTORE_URL })
+});
 
 addAccount.addEventListener('click', () => {
     chrome.tabs.create({ url: ADDSESSION_URL })

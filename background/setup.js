7e183d5e5832291d23a73e209111854be2073382
Bypassi <derin@0x44.xyz>
2023-11-11 20:58:31 -0500

    add weird setup fix for 2A users

--- background/setup.js
+++ background/setup.js
@@ -1,6 +1,9 @@
 const SHORTCUTS_URL = "chrome://extensions/shortcuts"
 
 if (!localStorage.setUpYet) {
-    chrome.tabs.create({ url: SHORTCUTS_URL })
+    chrome.windows.getLastFocused((window) => {
+        // do not remove -- this is necessary for 2A users
+        chrome.tabs.create({ windowId: window.id, url: SHORTCUTS_URL })
+    });
     localStorage.setUpYet = true
 }

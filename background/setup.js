2fd3a898f583ef77d7da0f3981afb49c3819a7ed
xcr15 <120267628+xcr15@users.noreply.github.com>
2023-11-12 11:17:28 -0500

    Update setup.js
    better functionality, you have to understand that this is something people will look at, create it with pride (no 2020 shit)

--- background/setup.js
+++ background/setup.js
@@ -1,9 +1,23 @@
 const SHORTCUTS_URL = "chrome://extensions/shortcuts"
-
-if (!localStorage.setUpYet) {
-    chrome.windows.getLastFocused((window) => {
-        // do not remove -- this is necessary for 2A users
-        chrome.tabs.create({ windowId: window.id, url: SHORTCUTS_URL })
+function hasSetup() {
+    return new Promise((resolve, reject) => {
+        chrome.storage.sync.get("setup", (items) => {
+            if (items) resolve(true);
+            else resolve(false);
+        });
     });
-    localStorage.setUpYet = true
 }
+function setStatus() {
+    chrome.storage.sync.set({ setup: true });
+}
+
+async function setup() {
+    let setupStatus = await hasSetup();
+    if (!setupStatus) {
+        chrome.windows.getLastFocused((window) => {
+         chrome.tabs.create({ windowId: window.id, url: SHORTCUTS_URL });
+         setStatus();
+    });
+  }
+} 
+setup();

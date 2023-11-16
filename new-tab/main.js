55166363f3c91f36294ffaa1ccb2d741f3a61801
LukasExists <91223726+lukasexists@users.noreply.github.com>
2023-11-16 14:58:13 +0000

    fixing things *again*

--- new-tab/main.js
+++ new-tab/main.js
@@ -11,57 +11,58 @@ const WEBSTORE_URL = "https://chromewebstore.google.com";
 const ADDSESSION_URL = "https://accounts.google.com/signin/v2/identifier?hl=en&continue=https%3A%2F%2Fwww.google.com%2F&ec=GAlAmgQ&flowName=GlifWebSignIn&flowEntry=AddSession";
 
 let [
-    contrast,
-    theme,
-    files,
-    help,
-    webStore,
-    addAccount,
-    move,
-    fullscreen
+  contrast,
+  theme,
+  files,
+  help,
+  webStore,
+  addAccount,
+  move,
+  fullscreen
 ] = document.querySelectorAll('svg')
 
 let time = document.querySelector('.time')
 let battery = document.querySelector('.battery')
 
 function setTheme() {
-    if (localStorage.useLightTheme) document.body.classList.add("light")
-        else document.body.classList.remove("light")
+  if (localStorage["useLightTheme"] == "light") document.body.classList.add("light")
+    else document.body.classList.remove("light")
 }
 
 setTheme()
 
 contrast.addEventListener('click', () => {
-    localStorage.useLightTheme = localStorage.useLightTheme ? false : true
-    setTheme()
+  if (localStorage.useLightTheme == "light") localStorage.useLightTheme = "dark"
+    else localStorage.useLightTheme = "light"
+  setTheme()
 })
 
 theme.addEventListener('click', () => {
-    chrome.tabs.create({ url: THEME_URL_1 })
-    chrome.tabs.create({ url: THEME_URL_2 })
+  chrome.tabs.create({ url: THEME_URL_1 })
+  chrome.tabs.create({ url: THEME_URL_2 })
 })
 
 files.addEventListener('click', () => {
-    chrome.tabs.create({}, (tab) => {
-        chrome.tabs.update(tab.id, { url: FILES_URL })
-    })
+  chrome.tabs.create({}, (tab) => {
+    chrome.tabs.update(tab.id, { url: FILES_URL })
+  })
 })
 
 help.addEventListener('click', () => {
-    chrome.tabs.create({ url: HELP_URL })
+  chrome.tabs.create({ url: HELP_URL })
 })
 
 webStore.addEventListener('click', () => {
-    let version = Number(navigator.appVersion.match(/Chrom(e|ium)\/([0-9]+)/)[2]);
-    if (version < 113) { // not sure if this is actually the version
-        alert("This web store may not supported by your version");
-    }
+  let version = Number(navigator.appVersion.match(/Chrom(e|ium)\/([0-9]+)/)[2]);
+  if (version < 113) { // not sure if this is actually the version
+    alert("This web store may not supported by your version");
+  }
 
-    chrome.tabs.create({ url: WEBSTORE_URL })
+  chrome.tabs.create({ url: WEBSTORE_URL })
 })
 
 addAccount.addEventListener('click', () => {
-    chrome.tabs.create({ url: ADDSESSION_URL })
+  chrome.tabs.create({ url: ADDSESSION_URL })
 })
 
 new DragController(move);

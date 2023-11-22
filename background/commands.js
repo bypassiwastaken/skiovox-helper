5a1d1b515e086a3774f52f6264cd40a4ced48d4a
Bypassi <derin@0x44.xyz>
2023-11-22 18:01:41 -0500

    quality of life updates to commands.js

--- background/commands.js
+++ background/commands.js
@@ -2,52 +2,57 @@ const VIEW_SOURCE_PREFIX = "view-source:"
 const HISTORY_URL = "chrome://history"
 const DOWNLOADS_URL = "chrome://downloads"
 
-function getRecent(callback) {
-  chrome.windows.getLastFocused({ populate: true }, (window) => {
-    callback({ window, tabs: window.tabs })
-  });
+function getRecentWindow() {
+  return new Promise(resolve => {
+    chrome.windows.getLastFocused({ populate: true }, resolve);
+  })
 }
 
-function cycleTabs(direction) {
-  getRecent(({ tabs }) => {
-    let currentTab = tabs.find((e) => e.active);
-    if (!currentTab) return;
+function cycleTabs(tabs, direction) {
+  let currentTab = tabs.find((e) => e.active);
+  if (!currentTab) return;
 
-    let index = currentTab.index + direction;
-    index = (index + tabs.length) % tabs.length // fix overflow
-    chrome.tabs.update(tabs[index].id, { active: true });
-  });
+  let index = currentTab.index + direction;
+  index = (index + tabs.length) % tabs.length // fix overflow
+  chrome.tabs.update(tabs[index].id, { active: true });
 }
 
-function exitFullscreen(window) {
-  if (window && window.state === chrome.windows.WindowState.FULLSCREEN) {
-    chrome.windows.update(window.id, { state: chrome.windows.WindowState.MAXIMIZED })
+async function onCommand(name, currentTab) {
+  let recentWindow = await getRecentWindow()
+  let recentTabs = recentWindow.tabs
+
+  // exit fullscreen
+  if (recentWindow && recentWindow.state === chrome.windows.WindowState.FULLSCREEN) {
+    chrome.windows.update(recentWindow.id, { state: chrome.windows.WindowState.MAXIMIZED })
   }
-}
 
-function onCommand(name, tab) {
+  function openTab(url) {
+    chrome.tabs.create({ windowId: recentWindow?.id, url });
+  }
+
   switch (name) {
     case "NEW_TAB":
-      getRecent(({ window }) => {
-        chrome.tabs.create({ windowId: window?.id });
-        exitFullscreen(window);
-      });
+      openTab();
+      break;
+
+    case "ACCESS_HISTORY":
+      openTab(HISTORY_URL);
+      break;
+
+    case "ACCESS_DOWNLOADS":
+      openTab(DOWNLOADS_URL);
       break;
 
     case "VIEW_SOURCE":
-      getRecent(({ tabs }) => {
-        let currentTab = tabs.find((e) => e.active);
-        if (!currentTab) return;
-        if (currentTab.url.startsWith(VIEW_SOURCE_PREFIX)) return;
+      if (!currentTab) return;
+      if (currentTab.url.startsWith(VIEW_SOURCE_PREFIX)) return;
 
-        chrome.tabs.create({ windowId: window?.id, url: VIEW_SOURCE_PREFIX + currentTab.url });
-        exitFullscreen(window);
-      });
+      openTab(VIEW_SOURCE_PREFIX + currentTab.url);
       break;
 
     case "CLOSE_TAB":
-      if (tab && tab.id !== chrome.tabs.TAB_ID_NONE) {
-        chrome.tabs.remove(tab.id);
+      if (currentTab && currentTab.id !== chrome.tabs.TAB_ID_NONE) {
+        chrome.tabs.remove(currentTab.id);
       }
       break;
 
@@ -64,35 +69,24 @@ function onCommand(name, tab) {
       break;
 
     case "CLOSE_WINDOW":
-      getRecent(({ window }) => {
-        if (window.focused) {
-          chrome.windows.remove(window.id);
-        }
-      });
-      break;
-
-    case "ACCESS_HISTORY":
-      chrome.tabs.create({ windowId: window?.id, url: HISTORY_URL });
-      break;
-
-    case "ACCESS_DOWNLOADS":
-      chrome.tabs.create({ windowId: window?.id, url: DOWNLOADS_URL });
+      if (recentWindow.focused) {
+        chrome.windows.remove(recentWindow.id);
+      }
       break;
 
     case "TAB_NEXT":
-      cycleTabs(1)
+      cycleTabs(recentTabs, 1)
       break;
 
     case "TAB_BACK":
-      cycleTabs(-1)
+      cycleTabs(recentTabs, -1)
       break;
 
     case "SWITCH_WINDOWS":
       chrome.windows.getAll((windows) => {
-        if (windows.length === 1) return;
-        getRecent(({ window }) => {
-          chrome.windows.update(window.id, { focused: false });
-        });
+        if (windows.length > 1) {
+          chrome.windows.update(recentWindow.id, { focused: false });
+        }
       })
       break;
 
@@ -105,19 +99,15 @@ function onCommand(name, tab) {
     case "CTRL_7":
     case "CTRL_8":
       let num = Number(name.split("_")[1]);
-      getRecent(({ tabs }) => {
-        let specifiedTab = tabs[num - 1];
-        if (!specifiedTab) return;
+      let specifiedTab = recentTabs[num - 1];
+      if (!specifiedTab) return;
 
-        chrome.tabs.update(specifiedTab.id, { active: true });
-      });
+      chrome.tabs.update(specifiedTab.id, { active: true });
       break;
 
     case "CTRL_9":
-      getRecent(({ tabs }) => {
-        let lastTab = tabs[tabs.length - 1];
-        chrome.tabs.update(lastTab.id, { active: true });
-      });
+      let lastTab = recentTabs[recentTabs.length - 1];
+      chrome.tabs.update(lastTab.id, { active: true });
       break;
   }
 }

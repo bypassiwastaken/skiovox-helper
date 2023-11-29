c48601960d6ce053e85cce25d1cd562c6022dab6
Echo <echo-the-coder@tuta.io>
2023-11-29 15:46:47 -0500

    Make time 24h

--- new-tab/time-display.js
+++ new-tab/time-display.js
@@ -1,18 +1,19 @@
 class TimeDisplay {
-    constructor(element) {
-        this.element = element
-        this.render()
-        this.startInterval()
-    }
+  constructor(element) {
+    this.element = element;
+    this.render();
+    this.startInterval();
+  }
 
-    startInterval() {
-        setInterval(this.render.bind(this), 50)
-    }
+  startInterval() {
+    setInterval(this.render.bind(this), 50);
+  }
 
-    render() { 
-       let date = new Date()
-       this.element.textContent = date.toLocaleTimeString()
-    }
+  render() {
+    let date = new Date();
+    // Use the toLocaleTimeString method with options to display in 24-hour format
+    this.element.textContent = date.toLocaleTimeString([], { hour12: false });
+  }
 }
 
-export { TimeDisplay }
+export { TimeDisplay };

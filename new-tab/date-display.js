86d73f0456186ff1baad50449f547b550a38238b
Bypassi <derin@0x44.xyz>
2023-12-07 22:27:54 -0500

    fix code (chad move)

--- new-tab/date-display.js
+++ new-tab/date-display.js
@@ -1,57 +1,57 @@
-class DateDisplay {
-    static DEFAULT_DATE = 1 // 1 is d/m/y, 2 is m/d/y
+const DateStyles = {
+    DEFAULT: 1,
+    SWAP_MD: 2
+}
 
+class DateDisplay {
     constructor(element) {
         this.element = element
-        this.setDate = this.getSavedDate()
+        this.dateStyle = this.getSavedStyle()
         this.render()
         this.startInterval()
     }
 
     startInterval() {
         this.element.addEventListener('click', this.onClickedDate.bind(this))
-        setInterval(this.render.bind(this), 50)
+        setInterval(this.render.bind(this), 100)
     }
 
-    render() { 
+    render() {
         let date = new Date()
-        if (this.setDate == 2) {
-           this.element.textContent = [
-           date.getMonth() + 1,
-           date.getDate(),
-           date.getFullYear()
-        ].join('/')
+
+        let parts = {
+            month: date.getMonth() + 1,
+            day: date.getDate(),
+            year: date.getFullYear()
         }
-        else {
-        this.element.textContent = [
-            date.getDate(),
-            date.getMonth() + 1,
-            date.getFullYear()
-        ].join('/')
+
+        if (this.dateStyle == 2) {
+            this.element.textContent = [
+                parts.month, parts.day, parts.year
+            ].join('/')
+        } else {
+            this.element.textContent = [
+                parts.day, parts.month, parts.year
+            ].join('/')
         }
     }
 
     onClickedDate() {
-        if (this.setDate == 1) {
-            this.setDate = 2
-        }
-        else {
-            this.setDate = 1
-        }
-        this.setSavedDate(this.setDate)
+        if (this.dateStyle === DateStyles.DEFAULT) {
+            this.dateStyle = DateStyles.SWAP_MD
+        } else {
+            this.dateStyle = DateStyles.DEFAULT
+        } // TODO: fix this middery
+
+        this.setSavedStyle(this.dateStyle)
     }
 
-    setSavedDate(value) {
-        localStorage.savedDate = value
+    setSavedStyle(value) {
+        localStorage.dateStyle = value
     }
 
-    getSavedDate() {
-        if (localStorage.savedDate = null) {
-            return localStorage.savedDate
-        }
-        else {
-            return DateDisplay.DEFAULT_DATE
-        }
+    getSavedStyle() {
+        return Number(localStorage.dateStyle) || DateStyles.DEFAULT
     }
 }
 
--- new-tab/time-display.js
+++ new-tab/time-display.js
@@ -1,13 +1,14 @@
-class TimeDisplay {
-    static DEFAULT_TIME = 1 
-    // 1 : 13:40:24
-    // 2 : 13:40   
-    // 3 : 1:40:24 PM
-    // 4 : 1:40 PM
+const TimeStyles = {
+    DEFAULT: 1,
+    NO_SECONDS: 2,
+    AM_PM: 3,
+    AM_PM_NO_SECONDS: 4
+}
 
+class TimeDisplay {
     constructor(element) {
         this.element = element
-        this.setTime = this.getSavedTime()
+        this.timeStyle = this.getSavedStyle()
         this.render()
         this.startInterval()
     }
@@ -19,48 +20,47 @@ class TimeDisplay {
 
     render() {
         let date = new Date()
-        var time
-        if (this.setTime <= 2) {
-            time = date.getHours()
+
+        let time = {
+            hours: String(date.getHours()),
+            hoursAmerican: String((date.getHours() - 1) % 12 + 1),
+            amPm: date.getHours() > 12 ? "PM" : "AM",
+            minutes: String(date.getMinutes()).padStart(2, '0'),
+            seconds: String(date.getSeconds()).padStart(2, '0'),
         }
-        else { 
-            time = date.getHours() % 12
-            if (time == 0) {
-                time = 12
-            }
+
+        switch (this.timeStyle) {
+            case TimeStyles.DEFAULT:
+                this.element.textContent = `${time.hours}:${time.minutes}:${time.seconds}`
+                break;
+            case TimeStyles.NO_SECONDS:
+                this.element.textContent = `${time.hours}:${time.minutes}`
+                break;
+            case TimeStyles.AM_PM:
+                this.element.textContent = `${time.hoursAmerican}:${time.minutes}:${time.seconds} ${time.amPm}`
+                break;
+            case TimeStyles.AM_PM_NO_SECONDS:
+                this.element.textContent = `${time.hoursAmerican}:${time.minutes} ${time.amPm}`
+                break;
         }
-        time += ":" + String(date.getMinutes()).padStart(2, '0')
-        if (this.setTime % 2 == 1) {
-            time += ":" + String(date.getSeconds()).padStart(2, '0')
-        }
-        if (this.setTime > 2) {
-            time += date.getHours() > 12 ? " PM" : " AM"
-        }
-        this.element.textContent = time
     }
 
     onClickedTime() {
-        console.log(this.setTime)
-        if (this.setTime >= 4) {
-            this.setTime = 1
+        if (this.timeStyle >= 4) {
+            this.timeStyle = 1
+        } else {
+            this.timeStyle += 1
         }
-        else {
-            this.setTime += 1
-        }
-        this.setSavedTime(this.setTime)
+
+        this.setSavedStyle(this.timeStyle)
     }
 
-    setSavedTime(value) {
-        localStorage.savedTime = value
+    setSavedStyle(value) {
+        localStorage.timeStyle = value
     }
 
-    getSavedTime() {
-        if (localStorage.savedTime) {
-            return localStorage.savedTime
-        }
-        else {
-            return TimeDisplay.DEFAULT_TIME
-        }
+    getSavedStyle() {
+        return Number(localStorage.timeStyle) || TimeStyles.DEFAULT
     }
 }
 

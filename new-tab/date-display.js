7ef146f5e6fc7a38e16683fcf288520054546e0c
DanTCTA <daniil.here@gmail.com>
2023-12-07 16:12:08 -0800

    Added universal time and date
    - When the date is clicked, it will now swap between 2 modes:
        - dd/mm/yyyy
        - mm/dd/yyyy
    
    - When the time is clicked, it will now swap between 4 modes:
        - 13:40:10
        - 13:40
        - 1:40:10 PM
        - 1:40 PM

--- new-tab/date-display.js
+++ new-tab/date-display.js
@@ -1,21 +1,57 @@
 class DateDisplay {
+    static DEFAULT_DATE = 1 // 1 is d/m/y, 2 is m/d/y
+
     constructor(element) {
         this.element = element
+        this.setDate = this.getSavedDate()
         this.render()
         this.startInterval()
     }
 
     startInterval() {
+        this.element.addEventListener('click', this.onClickedDate.bind(this))
         setInterval(this.render.bind(this), 50)
     }
 
     render() { 
-       let date = new Date()
-       this.element.textContent = [
+        let date = new Date()
+        if (this.setDate == 2) {
+           this.element.textContent = [
            date.getMonth() + 1,
            date.getDate(),
            date.getFullYear()
-       ].join('/')
+        ].join('/')
+        }
+        else {
+        this.element.textContent = [
+            date.getDate(),
+            date.getMonth() + 1,
+            date.getFullYear()
+        ].join('/')
+        }
+    }
+
+    onClickedDate() {
+        if (this.setDate == 1) {
+            this.setDate = 2
+        }
+        else {
+            this.setDate = 1
+        }
+        this.setSavedDate(this.setDate)
+    }
+
+    setSavedDate(value) {
+        localStorage.savedDate = value
+    }
+
+    getSavedDate() {
+        if (localStorage.savedDate = null) {
+            return localStorage.savedDate
+        }
+        else {
+            return DateDisplay.DEFAULT_DATE
+        }
     }
 }
 

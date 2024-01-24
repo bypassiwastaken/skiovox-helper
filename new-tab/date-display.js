e042e404558bf79ee8284ff8e192109d90cdddb7
zkayns <80561998+zkayns@users.noreply.github.com>
2024-01-23 19:29:32 -0500

    Update date-display.js

--- new-tab/date-display.js
+++ new-tab/date-display.js
@@ -3,20 +3,7 @@ class DateDisplay {
     constructor(element) {
         this.element = element
         this.dateStyle = this.getSavedStyle()
-        if (typeof this.dateStyle === 'string' || this.dateStyle instanceof String) {
-            // ok cool
-        } else {
-            if (this.dateStyle = DateStyles.DEFAULT) {
-            this.dateStyle = "1" 
-        }
-        else if (this.dateStyle = DateStyles.SWAP_MD) {
-            this.dateStyle = "2"
-        }
-        else if (this.dateStyle = DateStyles.YMD) {
-            this.dateStyle = "3"
-        }
-        }
-        this.setSavedStyle(this.dateStyle)
+        this.validityCheck()
         this.render()
         this.startInterval()
     }
@@ -65,8 +52,50 @@ class DateDisplay {
     }
 
     getSavedStyle() {
+        if (localStorage.dateStyle == null) {
+            autoDetect(chrome.i18n.getUILanguage())
+        }
         return localStorage.dateStyle || "1"
+    }
+    
+    validityCheck() {
+        if (parseInt(this.dateStyle) < 1) {
+            //this.autoDetect(chrome.i18n.getUILanguage())
+            this.dateStyle = "1";
+        }
+        if (typeof this.dateStyle === 'string' || this.dateStyle instanceof String) {
+            // ok cool
+        } else {
+            if (this.dateStyle = DateStyles.DEFAULT) {
+            this.dateStyle = "1" 
+        }
+        else if (this.dateStyle = DateStyles.SWAP_MD) {
+            this.dateStyle = "2"
+        }
+        else if (this.dateStyle = DateStyles.YMD) {
+            this.dateStyle = "3"
+        }
+        }
+        this.setSavedStyle(this.dateStyle)
     }
 }
 
 export { DateDisplay }
+/*const dmyLangs = ["el", "ml", "en-GB", "en-AU", "ca", "es", "es-419", "pt-BR", "pt-PT", "ro", "ru", "th", "kn", "mr", "uk", "pl", "ar", "fil", "he"]
+const mdyLangs = ["en-US"]
+const ymdLangs = ["ko", "vi", "zh-CN", "zh-TW", "sv", "sw"]*/
+    /*autoDetect(lang) {
+        if (this.dmyLangs.find(lang) > 0) {
+            this.dateStyle = "1"
+        } else if (this.mdyLangs.find(lang) > 0) {
+            this.dateStyle = "2"
+        } else if (this.ymdLangs.find(lang) > 0) {
+            this.dateStyle = "3"
+        } else {
+            this.dateStyle = "1"
+        }
+        this.setSavedStyle(this.dateStyle)
+        this.getSavedStyle()
+        this.render()
+    }
+}*/
